17959f0bc7d59a046b0935b56cf877b3f2120062
hoangtd <hoangtd@softdreams.vn>
2024-05-26 10:45:37 +0700

    init done

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ decorators:
   media-type-examples-override:
     operationIds:
 `
+let _initLog = {}
 
 const folderNames = [
     'assets',
@@ -67,12 +68,12 @@ const fileNames = {
 const sampleRequestHeaderTemplate = `@operationId@ sample request:
   summary: '@operationId@ sample request'
   value:
-    
+    convert json sample to yml then replace this line
 `
 const sampleResponseHeaderTemplate = `@operationId@ sample response:
   summary: '@operationId@ sample response'
   value:
-    
+    convert json sample to yml then replace this line
 `
 
 const init = async (runPath, url) => {
@@ -82,6 +83,9 @@ const init = async (runPath, url) => {
 
     _log(`Generating standard contents ...`)
     await decorateConfigFile(runPath, url)
+
+    _log(`Write log...`)
+    fs.writeFileSync(runPath + 'runLog.json', JSON.stringify(_initLog))
 }
 
 const decorateConfigFile = async (runPath, url) => {
@@ -94,13 +98,59 @@ const decorateConfigFile = async (runPath, url) => {
 
     // Download OpenAPI json content from url
     const jsonConfig = await downloadOpenAPIDescriptionFile(url)
-    let operationIds = getAllOperationIds(jsonConfig)
+    let apiSpecInfo = parseSpec(jsonConfig)
 
     // Generate sample files
-    genSampleAndInfoFiles(runPath, operationIds)
+    genSampleAndInfoFiles(runPath, apiSpecInfo)
 
     // Edit yaml config
+    let tmpInfo = {}
+    let tmpRequestResponse = {}
+    for (let item of apiSpecInfo) {
+        let id = item['operationId']
+        tmpInfo[id] = 'info/api/' + id + '_info.md'
+        // tmpInfo.push({[id]: descValue})
+        let requestValue = {}
+        if (item['requestContentType'])
+            requestValue[item['requestContentType']] = 'samples/request/' + id + '.yml'
+        let responseValue = {}
 
+        for (let res of item['responses']) {
+            let tmp = {}
+            tmp[res['contentType']] = `samples/response/${id}_${res['status']}.yml`
+            responseValue[res['status']] = tmp
+        }
+
+        if (Object.keys(requestValue).length > 0)
+            tmpRequestResponse[id] = {'request': requestValue, 'responses': responseValue}
+        else
+            tmpRequestResponse[id] = {'responses': responseValue}
+    }
+
+    const stack = [ymlConfig];
+
+    while (stack.length > 0) {
+        const currentObj = stack.pop();
+
+        for (const key in currentObj) {
+            if (currentObj.hasOwnProperty(key)) {
+                const value = currentObj[key];
+                if ((!['operation-description-override', 'media-type-examples-override'].includes(key)) && typeof value === 'object') {
+                    stack.push(value);
+                } else if (key === 'operation-description-override') {
+                    currentObj[key]['operationIds'] = tmpInfo
+                } else if (key === 'media-type-examples-override') {
+                    currentObj[key]['operationIds'] = tmpRequestResponse
+                }
+            }
+        }
+    }
+
+    let newContent = yaml.dump(ymlConfig)
+    fs.writeFileSync(runPath + 'redocly.yml', newContent)
+    _log(`
+        ------ Modified redocly config file ------
+    `)
 }
 
 const downloadOpenAPIDescriptionFile = (url) => {
@@ -132,18 +182,75 @@ const downloadOpenAPIDescriptionFile = (url) => {
     });
 }
 
-const genSampleAndInfoFiles = (currentPath, operationIds) => {
+const parseSpec = (openApiSpec) => {
+    const result = [];
+
+    // Duyệt qua tất cả các paths
+    for (const path in openApiSpec.paths) {
+        if (openApiSpec.paths.hasOwnProperty(path)) {
+            const methods = openApiSpec.paths[path];
+
+            // Duyệt qua tất cả các phương thức HTTP
+            for (const method in methods) {
+                if (methods.hasOwnProperty(method)) {
+                    const operation = methods[method];
+
+                    const entry = {
+                        operationId: operation.operationId,
+                        responses: []
+                    };
+
+                    // Nếu có requestBody, thêm requestContentType
+                    if (operation.requestBody && operation.requestBody.content) {
+                        entry.requestContentType = Object.keys(operation.requestBody.content)[0];
+                    }
+
+                    // Duyệt qua các responses
+                    for (const status in operation.responses) {
+                        if (operation.responses.hasOwnProperty(status)) {
+                            const response = operation.responses[status];
+
+                            if (response.content) {
+                                const contentType = Object.keys(response.content)[0];
+                                entry.responses.push({
+                                    status: status,
+                                    contentType: contentType
+                                });
+                            }
+                        }
+                    }
+
+                    result.push(entry);
+                }
+            }
+        }
+    }
+
+    return result
+}
+
+const genSampleAndInfoFiles = (currentPath, apiSpecInfo) => {
     const infoBasePath = currentPath + '/info/api/'
     const sampleBasePath = currentPath + '/samples/'
-    for (let id of operationIds) {
+    for (let item of apiSpecInfo) {
+        let id = item['operationId']
+        _initLog[id] = {}
         let infoFile = infoBasePath + id + '_info.md'
-        let sampleRequest = sampleBasePath + '/request/' + id + '.yml'
-        let sampleResponse = sampleBasePath + '/response/' + id + '.yml'
-        let requestHeader = sampleRequestHeaderTemplate.replace(new RegExp('@operationId@', 'g'), id)
-        let responseHeader = sampleResponseHeaderTemplate.replace(new RegExp('@operationId@', 'g'), id)
         fs.writeFileSync(infoFile, '')
-        fs.writeFileSync(sampleRequest, requestHeader)
-        fs.writeFileSync(sampleResponse, responseHeader)
+        _initLog[id]['info'] = 1
+        if (item['requestContentType']) {
+            let sampleRequest = sampleBasePath + '/request/' + id + '.yml'
+            // let requestHeader = sampleRequestHeaderTemplate.replace(new RegExp('@operationId@', 'g'), id)
+            fs.writeFileSync(sampleRequest, '')
+            _initLog[id]['request'] = 1
+        }
+        for (let res of item['responses']) {
+            let sampleResponse = `${sampleBasePath}/response/${id}_${res['status']}.yml`
+            // let responseHeader = sampleResponseHeaderTemplate.replace(new RegExp('@operationId@', 'g'), id)
+            fs.writeFileSync(sampleResponse, '')
+            _initLog[id]['responses'] = {}
+            _initLog[id]['responses'][res['status']] = 1
+        }
     }
     _log(`
         ------ Generated sample request, response, info files ------
@@ -186,68 +293,69 @@ const addDecoratorsForMainConfig = (runPath) => {
         _tmpRequestResponse.push({[id]: combination})
     }
 
-        const stack = [ymlConfig];
+    const stack = [ymlConfig];
 
-        while (stack.length > 0) {
-            const currentObj = stack.pop();
+    while (stack.length > 0) {
+        const currentObj = stack.pop();
 
-            for (const key in currentObj) {
-                if (currentObj.hasOwnProperty(key)) {
-                    const value = currentObj[key];
-                    if ((!['operation-description-override', 'media-type-examples-override'].includes(key)) && typeof value === 'object') {
-                        stack.push(value);
-                    } else if (key === 'operation-description-override') {
-                        currentObj[key]['operationIds'] = _tmpDesc
-                    } else if (key === 'media-type-examples-override') {
-                        currentObj[key]['operationIds'] = _tmpRequestResponse
-                    }
+        for (const key in currentObj) {
+            if (currentObj.hasOwnProperty(key)) {
+                const value = currentObj[key];
+                if ((!['operation-description-override', 'media-type-examples-override'].includes(key)) && typeof value === 'object') {
+                    stack.push(value);
+                } else if (key === 'operation-description-override') {
+                    currentObj[key]['operationIds'] = _tmpDesc
+                } else if (key === 'media-type-examples-override') {
+                    currentObj[key]['operationIds'] = _tmpRequestResponse
                 }
             }
         }
-
-        console.log(yaml.dump(ymlConfig))
     }
+
+    console.log(yaml.dump(ymlConfig))
+}
 
 // decorateConfigFile('./test/redocly.yml', 'http://localhost:8080/v3/api-docs').catch()
-    addDecoratorsForMainConfig('./test/')
+// addDecoratorsForMainConfig('./test/')
 
-    const createStructure = (basePath) => {
-        // Check path exists?
-        if (!fs.existsSync(basePath)) {
-            _log(`Path does not exist`)
-            return
-        }
+const createStructure = (basePath) => {
+    // Check path exists?
+    if (!fs.existsSync(basePath)) {
+        _log(`Path does not exist`)
+        return
+    }
 
-        // Check folder is empty or not?
-        const files = fs.readdirSync(basePath);
-        if (files.length !== 0) {
-            _log(`Root folder is not empty`)
-            return
-        }
+    // Check folder is empty or not?
+    const files = fs.readdirSync(basePath);
+    if (files.length !== 0) {
+        _log(`Root folder is not empty`)
+        return
+    }
 
-        // create folders
-        for (let folder of folderNames) {
-            const currentPath = path.join(basePath, folder)
-            fs.mkdirSync(currentPath)
-            _log(`Created folder: ${folder}`)
-        }
+    // create folders
+    for (let folder of folderNames) {
+        const currentPath = path.join(basePath, folder)
+        fs.mkdirSync(currentPath)
+        _log(`Created folder: ${folder}`)
+    }
 
-        // create files
-        for (const [fileName, content] of Object.entries(fileNames)) {
-            const filePath = path.join(basePath, fileName)
-            fs.writeFileSync(filePath, content)
-            _log(`Created file: ${filePath}`)
-        }
-        _log(`
+    // create files
+    for (const [fileName, content] of Object.entries(fileNames)) {
+        const filePath = path.join(basePath, fileName)
+        fs.writeFileSync(filePath, content)
+        _log(`Created file: ${filePath}`)
+    }
+    _log(`
         ------ Init project structure done ------
     `)
-    }
+}
 
-    const _log = (message) => {
-        console.log(message)
-    }
+const _log = (message) => {
+    console.log(message)
+}
 
-    module.exports = {
-        createStructure,
-        init
-    }
+module.exports = {
+    createStructure,
+    init,
+    downloadOpenAPIDescriptionFile
+}
--- test.js
+++ test.js
@@ -1,3 +1,53 @@
 const runner = require('./index')
 // runner.createStructure('D:\\14-APIDOC-GENERATOR\\test')
 runner.init('D:\\14-APIDOC-GENERATOR\\test\\', 'http://localhost:8080/v3/api-docs').catch(e => console.log(e))
+
+// const parseSpec = async () => {
+//     let openApiSpec = await runner.downloadOpenAPIDescriptionFile('http://localhost:8080/v3/api-docs')
+//     const result = [];
+//
+//     // Duyệt qua tất cả các paths
+//     for (const path in openApiSpec.paths) {
+//         if (openApiSpec.paths.hasOwnProperty(path)) {
+//             const methods = openApiSpec.paths[path];
+//
+//             // Duyệt qua tất cả các phương thức HTTP
+//             for (const method in methods) {
+//                 if (methods.hasOwnProperty(method)) {
+//                     const operation = methods[method];
+//
+//                     const entry = {
+//                         operationId: operation.operationId,
+//                         responses: []
+//                     };
+//
+//                     // Nếu có requestBody, thêm requestContentType
+//                     if (operation.requestBody && operation.requestBody.content) {
+//                         entry.requestContentType = Object.keys(operation.requestBody.content)[0];
+//                     }
+//
+//                     // Duyệt qua các responses
+//                     for (const status in operation.responses) {
+//                         if (operation.responses.hasOwnProperty(status)) {
+//                             const response = operation.responses[status];
+//
+//                             if (response.content) {
+//                                 const contentType = Object.keys(response.content)[0];
+//                                 entry.responses.push({
+//                                     status: status,
+//                                     contentType: contentType
+//                                 });
+//                             }
+//                         }
+//                     }
+//
+//                     result.push(entry);
+//                 }
+//             }
+//         }
+//     }
+//
+//     console.log(result);
+// }
+
+// parseSpec().catch(e => console.log(e))
