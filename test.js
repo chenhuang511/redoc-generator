aa642fc1f462efbbb4224c956d18f0ae7727ce30
hoangtd <hoangtd@softdreams.vn>
2024-05-27 09:52:12 +0700

    remove test, cli is ready

--- test.js
+++
@@ -1,3 +0,0 @@
-const runner = require('./index')
-// runner.init('D:\\14-APIDOC-GENERATOR\\test\\', 'http://localhost:8080/v3/api-docs').catch(e => console.log(e))
-runner.update('D:\\14-APIDOC-GENERATOR\\test\\').catch(e => console.log(e))
